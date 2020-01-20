9a4233d1f3c497e7929473c7c715740ffdb74c9d
Thor K. H <thor@roht.no>
2020-01-20 10:12:07 +0100

    mmonitors: update shitty monitor utility

--- local/bin/mmonitors.sh
+++ local/bin/mmonitors.sh
@@ -84,11 +84,11 @@ set_work() {
 		--output eDP-1 --off \
 		--output DP-1 --off --output HDMI-1 --off \
 		--output DP-2 --off \
-		--output HDMI-2 --mode 2560x1440 --pos 0x0 --rotate normal \
+		--output HDMI-2 --mode 2560x1440 --pos 0x0 --rotate right \
 		--output DP-2-1 --off \
 		--output DP-2-2 --off \
 		--output DP-2-3 --off \
-		--output DP-1-1 --mode 2560x1440 --pos 5120x0 --rotate normal \
+		--output DP-1-1 --mode 2560x1440 --pos 5120x0 --rotate left \
 		--output DP-1-2 --primary --mode 2560x1440 --pos 2560x0 --rotate normal \
 		--output DP-1-3 --off
 	feh --bg-fill --no-fehbg --randomize ~/Dropbox/Variert/Bakgrunner/up-close/
