ecdcacd32f8b4874e98aaf8ed75ae69ea06996fc
Thor K. Høgås <thor@roht.no>
2022-11-18 12:12:30 +0100

    nvim: add silicon plugin for rendering fun
    See https://github.com/krivahtoo/silicon.nvim.
    
    Note that
    
      system_profiler SPFontsDataType
    
    is the way to get the list of recognised fonts on macOS, using the
    "family" attribute to pass along to Silicon.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,4 +1,11 @@
-return require('packer').startup(function(use)
+require('packer').startup(function(use)
 	-- Packer itself
 	use 'wbthomason/packer.nvim'
+
+	use {'krivahtoo/silicon.nvim', run = './install.sh'}
 end)
+
+require('silicon').setup({
+  font = 'Fantasque Sans Mono=16',
+  theme = 'Monokai Extended',
+})
