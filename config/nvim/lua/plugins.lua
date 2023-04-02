f62df234f0f630d32397b62ceed78e071a25c22f
Thor K. Høgås <thor@roht.no>
2023-04-02 21:30:35 +0100

    refactor(nvim): move treesitter configuration
    This moves, as with silicon.lua, the configuration into plugins.lua
    itself. There are downsides to this approach, but should configuration
    files grow too much then we'll load them directly from the config block
    instead in order to separate them out.
    
    Alternatively, we could try to load them dynamically by checking for the
    existance of such yummy files.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -17,8 +17,45 @@ require('packer').startup(function(use)
 	---- core lsp
 	use 'neovim/nvim-lspconfig'
 	---- enhanced highlighting, incl. additional treesitter context
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } 
-	use 'nvim-treesitter/nvim-treesitter-context'
+	use { 'nvim-treesitter/nvim-treesitter', 
+		run = ':TSUpdate',
+		config = function() 
+			require'nvim-treesitter.configs'.setup {
+				-- A list of parser names, or "all"
+				ensure_installed = { "bash", "lua", "rust", "vim", "go", "python" },
+
+				-- Install parsers synchronously (only applied to `ensure_installed`)
+				sync_install = false,
+
+				highlight = {
+					-- `false` will disable the whole extension
+					enable = true,
+				}
+			}
+		end
+	} 
+	use {
+		'nvim-treesitter/nvim-treesitter-context',
+		after = 'nvim-treesitter/nvim-treesitter',
+		config = function() 
+			require'treesitter-context'.setup {
+				-- Use the highlight group TreesitterContext to change the colors of the
+				-- context. Per default it links to NormalFloat. Use the highlight group
+				-- TreesitterContextLineNumber to change the colors of the context line
+				-- numbers if line_numbers is set. Per default it links to LineNr. Use the
+				-- highlight group TreesitterContextBottom to change the highlight of the
+				-- last line of
+				-- the context window. By default it links to NONE. 
+				--
+				-- However, you can use this to create a border by applying an underline
+				-- highlight, e.g:
+				-- 
+				-- hi TreesitterContextBottom gui=underline guisp=Grey
+				enable = true, 
+			}
+		end
+	}
+
 
 	-- file tree viewer with dev icons
 	use {
