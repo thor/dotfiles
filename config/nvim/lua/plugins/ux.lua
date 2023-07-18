d572cdc242fecd3f7f423bdf750fbdff3f3522e6
Thor K. Høgås <thor@roht.no>
2023-07-18 23:07:08 +0200

    feat(nvim): add initial which-key setup

---
+++ config/nvim/lua/plugins/ux.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    -- help me be better at remembering my own keys
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {}
+  }
+}
