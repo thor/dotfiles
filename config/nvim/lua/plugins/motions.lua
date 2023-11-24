4734dfbc0dba2bd6e2343c718de2ff77ba84f99a
Thor K. Høgås <thor@roht.no>
2023-11-24 14:41:40 +0100

    feat(nvim): add leap as sneak-replacement
    Adds the ability to use s and S to move around with only a few keypresses.

---
+++ config/nvim/lua/plugins/motions.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    -- it's sneak, but better
+    'ggandor/leap.nvim',
+    enabled = true,
+    keys = {
+      { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+      { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+      { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    },
+    lazy = false,
+    conf = function(_, opts)
+      local leap = require('leap')
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      --leap.add_repeat_mappings(';', ',', {
+      --  relative_directions = true,
+      --  modes = {'n', 'x', 'o'},
+      --})
+    end
+  }
+}
