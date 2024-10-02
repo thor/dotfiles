25796ea34f9c7cb100e4fbe9c0af6c2fd02383cc
Thor K. Høgås <thor@roht.no>
2024-10-02 14:04:31 +0200

    feat(nvim): add telescope to lookup files, help, symbols and more

--- config/nvim/lua/plugins/ux.lua
+++ config/nvim/lua/plugins/ux.lua
@@ -26,5 +26,35 @@ return {
     keys = {
       { "<leader>gg", "<cmd>LazyGit<CR>", desc = "open git overview" },
     },
-  }
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'neovim/nvim-lspconfig',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build =
+        'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+      }
+    },
+    keys = {
+      { "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find files" },
+      { "<leader>fh", function() require("telescope.builtin").help_tags() end,  desc = "Find help" },
+      { "<leader>fs", function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end,  desc = "Find symbol in workspace" },
+      { "<leader>ft", function() require("telescope.builtin").treesitter() end,  desc = "Find symbol from treesitter" },
+      { "<leader>fd", function() require("telescope.builtin").diagnostics() end,  desc = "Find diagnostics" },
+      { "<leader>gs", function() require("telescope.builtin").git_status() end,  desc = "Open Git status" },
+    },
+    opts = {
+      defaults = {
+        winblend = 1
+      },
+    },
+    config = function (_, opts)
+      require('telescope').setup{opts}
+      require('telescope').load_extension('fzf')
+    end
+  },
 }
