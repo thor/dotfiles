9d1e36a5f67b56c2afbc2a8c9fb1d90cf11bcbb3
Thor K. Høgås <thor@roht.no>
2025-04-01 12:42:59 +0200

    feat(nvim): add rainbow indentation

--- config/nvim/lua/plugins/ux.lua
+++ config/nvim/lua/plugins/ux.lua
@@ -25,7 +25,31 @@ return {
     -- show beautiful indentation lines
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    opts = {},
+    opts = function()
+      local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
+      }
+
+      local hooks = require "ibl.hooks"
+      -- create the highlight groups in the highlight setup hook, so they are reset
+      -- every time the colorscheme changes
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#6e171e" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#765517" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#0c497a" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#603d1d" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#3b5727" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#5a1b6d" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#1e4c52" })
+      end)
+      return { indent = { highlight = highlight } }
+    end,
   },
   {
     -- enhanced inputs and selection interfaces
