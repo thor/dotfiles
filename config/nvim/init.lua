eba62daa56b8a818098f99e37e844b2063d34aa8
Thor K. Høgås <thor@roht.no>
2026-03-09 16:34:42 +0100

    fix(nvim): don't use clip.exe for everyone

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -61,20 +61,21 @@ vim.g.mapleader   = " "
 -- Set local leader to be the backspace, somewhat convenient
 vim.g.localleader = "\\"
 
--- vim.g.clipboard = "clip"
-vim.g.clipboard = {
-      name = "WslClipboard",
-      copy = {
-         ['+'] = "clip.exe",
-         ['*'] = "clip.exe",
-       },
-      paste = {
-         ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-         ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+if vim.fn.has('wsl') == 1 then
+  vim.g.clipboard = {
+        name = "WslClipboard",
+        copy = {
+           ['+'] = "clip.exe",
+           ['*'] = "clip.exe",
+         },
+        paste = {
+           ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+           ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
 
-      },
-      cache_enabled = 0,
-    }
+        },
+        cache_enabled = 0,
+      }
+end
 -- Get the guicursor of my dreams
 vim.o.guicursor = vim.o.guicursor .. ",a:blinkwait175-blinkoff100-blinkon170"
 
