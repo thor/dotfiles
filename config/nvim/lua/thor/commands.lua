ce50e35b633dc25cfe09332e5b66dfb6bb51e79f
Thor K. Høgås <thor@roht.no>
2026-04-13 13:06:16 +0200

    clean: remove custom buffer delete command due bufkill

--- config/nvim/lua/thor/commands.lua
+++ config/nvim/lua/thor/commands.lua
@@ -2,6 +2,3 @@
 -------------------------------------------------------------------------------
 -- Commands
 
--- Add buffer deletion command for more-than-one windows and buffers
-vim.api.nvim_create_user_command('Bd', 'bp | sp | bn | bd', {})
-
